62b837385b9ce5a30549173d1fbb16bc62420602
Your Name <rahulmohature0421@gmail.com>
2024-08-16 13:50:35 +0530

    added new code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import React, { VFC } from 'react'
 import { LinkIconButton } from './LinkIconButton'
-import { TCanvas } from './three/TCanvas'
+// import { TCanvas } from './three/TCanvas'
 
 export const App : VFC = () => {
     return(
         <div style={{width: '100vw', height: '100vh'}}>
-            <TCanvas />
+            {/* <TCanvas /> */}
             <LinkIconButton imagePath ="/assets/icons/github.svg" linkPath= "https://github.com/6rahul9/sphere"/>
         </div>
     )
