e3450b50c3280902076a9d940ef6481bf1ded275
Your Name <rahulmohature0421@gmail.com>
2024-08-17 14:08:09 +0530

    added new code

--- src/modules/gui.ts
+++ src/modules/gui.ts
@@ -20,15 +20,15 @@ export class GUIController{
 
     private _getGui = (folderName : string | undefined) => {
         let gui = this._gui
-        if(foldrName){
+        if(folderName){
             gui = this._folder(folderName)
         }else if (this._currentFolderName){
             gui = this._folder(this._currentFolderName)
         }
     }
 
-    private _folder = (title: string)=> {
-        let _flder = this._gui._folders.find(f =>f._title === title )
+    private _folder = (title: string) => {
+        let _folder = this._gui.folders.find(f => f._title === title )
         if(!folder) folder = this._gui.addFolder(title)
             return folder
     }
