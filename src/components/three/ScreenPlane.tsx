9cf7366690a88efefd8b37ba4629e2e1776fd9bd
Your Name <rahulmohature0421@gmail.com>
2024-08-20 17:56:24 +0530

    new code added

--- src/components/three/ScreenPlane.tsx
+++ src/components/three/ScreenPlane.tsx
@@ -3,3 +3,17 @@ import * as THREE from 'three'
 import { Plane } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { fresnel, rotate } from '../../modules/glsl';
+
+export const ScreenPlane: VFC = () => {
+    const shader : THREE.Shader ={
+        uniforms:{
+            u_time : { value : 0 },
+            u_aspect: { value : 0 },
+            u_mouse: { value : new THREE.Vector2(0, 0) }
+        },
+        vertexShader : vertexShader,
+        fragmentShader: fragmentShader 
+    }
+
+    
+}
