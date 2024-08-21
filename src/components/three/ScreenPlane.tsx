083ab09183d998b658627bea3fbfa423794f86b3
Your Name <rahulmohature0421@gmail.com>
2024-08-21 14:16:11 +0530

    new code added

--- src/components/three/ScreenPlane.tsx
+++ src/components/three/ScreenPlane.tsx
@@ -16,5 +16,10 @@ export const ScreenPlane: VFC = () => {
     }
 
     const vec = new THREE.Vector2()
+    useFrame(({ size, mouse }) => {
+        shader.uniforms.u_time.value += 0.005
+        shader.uniforms.u_aspect.value = size.width / size.height
+        shader.uniforms.u_mouse.value.lerp(vec.set(mouse.x / 2, mouse.y / 2, 0.05)
+    })
     
 }
