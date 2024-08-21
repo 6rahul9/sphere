e6bade96d2e521228006a6bb32d7073391816134
Your Name <rahulmohature0421@gmail.com>
2024-08-21 14:54:02 +0530

    new code added

--- src/components/three/ScreenPlane.tsx
+++ src/components/three/ScreenPlane.tsx
@@ -58,4 +58,11 @@ const fragmentShader = `
     float opUnion (float d1, float d2){return min(d1, d2)}
     float opUnion (float d1, float d2){return max(-d1, d2)}
     float opUnion (float d1, float d2){return max(d1, d2)}
+
+    flaot opSmoothSubtraction(flaot d1, flaot d2, float k){
+    float h = clamp(0.5+0.5*(d2+d1))/k, 0.0, 1.0)
+        return mix (d2, -d1, h) - k*h*(1.0 - h)
+    }
+
+    
 `
