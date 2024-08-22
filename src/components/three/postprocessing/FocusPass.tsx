18548984b0cb564a843c3893b8f504f24fcf7104
Your Name <rahulmohature0421@gmail.com>
2024-08-22 22:29:29 +0530

    new code addded

--- src/components/three/postprocessing/FocusPass.tsx
+++ src/components/three/postprocessing/FocusPass.tsx
@@ -4,3 +4,24 @@ import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 
 import { useFrame } from '@react-three/fiber'
 import { GUIController } from '../../../modules/gui';
+
+const datas = {
+    enabled: true,
+    focus : -0.05,
+    blur : 1,
+    samples: 20
+}
+
+export const Focuspass : VFC = () =>{
+    const passRef = useref<ShaderPass>(null)
+
+    const gui = GUIController.instance.setFolder('Focus')
+    gui.setOpen(false);
+    gui.addCheckBox(datas, 'enabled')
+    gui.addNumericSlider(datas, 'exposure', -0.5, 0.5, 0.01)
+    gui.addNumericSlider(datas, 'strength', 0, 1, 0.01)
+
+    gui.addNumericSlider(datas, 'threshold', 10, 100, 10)
+
+    
+}
