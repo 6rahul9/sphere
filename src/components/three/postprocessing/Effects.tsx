0b7b4a158dcdac39f738cab41141dcf31f1e5915
Your Name <rahulmohature0421@gmail.com>
2024-08-22 13:18:00 +0530

    new code added

--- src/components/three/postprocessing/Effects.tsx
+++ src/components/three/postprocessing/Effects.tsx
@@ -2,6 +2,12 @@ import { useEffect, useRef, VFC } from "react";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
-import { GammaCoorectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
 import { extend ,useFrame, useThree } from "@react-three/fiber";
 
+extend ({ EffectComposer, RenderPass, ShaderPass})
+
+type EffectsProps = {
+    children : React.ReactNode
+    sRGBCorrection? : boolean;  
+}
