132b8523439e464be6b832f49d5764f59e459acd
Your Name <rahulmohature0421@gmail.com>
2024-08-26 18:03:17 +0530

    fixed the errors in all these files

--- src/components/three/postprocessing/Effects.tsx
+++ src/components/three/postprocessing/Effects.tsx
@@ -1,37 +1,75 @@
-import { Children, useEffect, useRef, VFC } from "react";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
-import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
-import { extend ,useFrame, useThree } from "@react-three/fiber";
-import { Camera, Scene } from "three";
+// import { Children, useEffect, useRef, VFC } from "react";
+// import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+// import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+// import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+// import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
+// import { extend ,useFrame, useThree } from "@react-three/fiber";
+// import { Camera, Scene } from "three";
 
-extend ({ EffectComposer, RenderPass, ShaderPass})
+// extend ({ EffectComposer, RenderPass, ShaderPass})
+
+// type EffectsProps = {
+//     children : React.ReactNode
+//     sRGBCorrection? : boolean;  
+// }
+
+// export const Effects : VFC<EffectsProps> = props =>{
+//     const { children, sRGBCorrection } = props
+
+//     const composerRef = useRef<EffectComposer>(null)
+//     const { gl, scene, camera, size } = useThree()
+
+//     useEffect(() =>{
+//         composerRef.current!.setSize(size.width, size.height)
+//     }, [size])
+
+//     useFrame(() =>{
+//         composerRef.current!.render()
+//     }, 1)
+
+//     return(
+//         <effectComposer ref={composerRef} args={[gl]}>
+//             <renderPass attachArray="passes" args={[scene, camera]}/>
+//             {sRGBCorrection && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
+//             {children}
+//         </effectComposer>
+//     )
+// }
+
+
+import { useEffect, useRef, VFC } from 'react';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader';
+import { extend, useFrame, useThree } from '@react-three/fiber';
+
+extend({ EffectComposer, RenderPass, ShaderPass })
 
 type EffectsProps = {
-    children : React.ReactNode
-    sRGBCorrection? : boolean;  
+	children: React.ReactNode
+	sRGBCorrection?: boolean
 }
 
-export const Effects : VFC<EffectsProps> = props =>{
-    const { children, sRGBCorrection } = props
+export const Effects: VFC<EffectsProps> = props => {
+	const { children, sRGBCorrection } = props
 
-    const composerRef = useRef<EffectComposer>(null)
-    const { gl, scene, camera, size } = useThree()
+	const composerRef = useRef<EffectComposer>(null)
+	const { gl, scene, camera, size } = useThree()
 
-    useEffect(() =>{
-        composerRef.current!.setSize(size.width, size.height)
-    }, [size])
+	useEffect(() => {
+		composerRef.current!.setSize(size.width, size.height)
+	}, [size])
 
-    useFrame(() =>{
-        composerRef.current!.render()
-    }, 1)
+	useFrame(() => {
+		composerRef.current!.render()
+	}, 1)
 
-    return(
-        <effectComposer ref={composerRef} args={[gl]}>
-            <renderPass attachArray="passes" args={[scene, camera]}/>
-            {sRGBCorrection && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
-            {children}
-        </effectComposer>
-    )
+	return (
+		<effectComposer ref={composerRef} args={[gl]}>
+			<renderPass attachArray="passes" args={[scene, camera]} />
+			{sRGBCorrection && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
+			{children}
+		</effectComposer>
+	)
 }
