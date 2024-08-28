2c31fb94c4d0d004de1d900516c52d2a59c2c8b2
Your Name <rahulmohature0421@gmail.com>
2024-08-28 11:25:08 +0530

    .

--- src/components/three/postprocessing/Effects.tsx
+++ src/components/three/postprocessing/Effects.tsx
@@ -1,40 +1,4 @@
-// import { Children, useEffect, useRef, VFC } from "react";
-// import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-// import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-// import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
-// import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
-// import { extend ,useFrame, useThree } from "@react-three/fiber";
-// import { Camera, Scene } from "three";
 
-// extend ({ EffectComposer, RenderPass, ShaderPass})
-
-// type EffectsProps = {
-//     children : React.ReactNode
-//     sRGBCorrection? : boolean;  
-// }
-
-// export const Effects : VFC<EffectsProps> = props =>{
-//     const { children, sRGBCorrection } = props
-
-//     const composerRef = useRef<EffectComposer>(null)
-//     const { gl, scene, camera, size } = useThree()
-
-//     useEffect(() =>{
-//         composerRef.current!.setSize(size.width, size.height)
-//     }, [size])
-
-//     useFrame(() =>{
-//         composerRef.current!.render()
-//     }, 1)
-
-//     return(
-//         <effectComposer ref={composerRef} args={[gl]}>
-//             <renderPass attachArray="passes" args={[scene, camera]}/>
-//             {sRGBCorrection && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
-//             {children}
-//         </effectComposer>
-//     )
-// }
 
 
 import { useEffect, useRef, VFC, forwardRef } from 'react';
