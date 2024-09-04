2a161a38a1e47716da9f93040612a6978bd9e919
Your Name <rahulmohature0421@gmail.com>
2024-09-04 18:31:44 +0530

    ????

--- src/components/three/postprocessing/Effects.tsx
+++ src/components/three/postprocessing/Effects.tsx
@@ -1,45 +1,7 @@
 
 
 
-// import { useEffect, useRef, FC, forwardRef } from 'react';
-// import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-// import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-// import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader';
-// import { extend, useFrame, useThree } from '@react-three/fiber';
-
-// extend({ EffectComposer, RenderPass, ShaderPass })
-
-// type EffectsProps = {
-// 	children: React.ReactNode
-// 	sRGBCorrection?: boolean
-// }
-
-// export const Effects: FC<EffectsProps> = props => {
-// 	const { children, sRGBCorrection } = props
-
-// 	const composerRef = useRef<EffectComposer>(null)
-// 	const { gl, scene, camera, size } = useThree()
-
-// 	useEffect(() => {
-// 		composerRef.current!.setSize(size.width, size.height)
-// 	}, [size])
-
-// 	useFrame(() => {
-// 		composerRef.current!.render()
-// 	}, 1)
-
-// 	return (
-// 		<effectComposer ref={composerRef} args={[gl]}>
-// 			<renderPass attachArray="passes" args={[scene, camera]} />
-// 			{sRGBCorrection && <shaderPass attachArray="passes" args={[GammaCorrectionShader]} />}
-// 			{children}
-// 		</effectComposer>
-// 	)
-// }
-
-
-import { useEffect, useRef, VFC } from 'react';
+import { useEffect, useRef, FC, forwardRef } from 'react';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
@@ -53,7 +15,7 @@ type EffectsProps = {
 	sRGBCorrection?: boolean
 }
 
-export const Effects: VFC<EffectsProps> = props => {
+export const Effects: FC<EffectsProps> = props => {
 	const { children, sRGBCorrection } = props
 
 	const composerRef = useRef<EffectComposer>(null)
